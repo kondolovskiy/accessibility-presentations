6bac243f326eeb09e5d06300e2fcdb3bbb2351f0
Viktor Kondolovskyi <viktor@clioonline.dk>
2018-11-20 17:15:14 +0200

    Updates

---
+++ scripts/client.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', () => {
+	const slides = document.querySelectorAll('.slide');
+	console.log(slides.length);
+});
+
+const socket = new WebSocket('ws://localhost:3001');
+
+socket.onopen = function() {
+	console.log("Connected");
+};
+
+socket.onclose = function(event) {
+	if (event.wasClean) {
+		console.log('Connection are closed');
+	} else {
+		console.log('Bad connection'); // например, "убит" процесс сервера
+	}
+	console.log('Code: ' + event.code + ' reason: ' + event.reason);
+};
+
+socket.onmessage = function(event) {
+	console.log("received data " + event.data);
+	if (event.data) {
+		let hash = +window.location.hash.split('#')[1];
+
+		if (!Number.isInteger(hash)) {
+			hash = '#cover';
+		} else {
+			hash = '#' + ++hash;
+		}
+
+		window.location.hash = hash;
+	}
+};
+
+socket.onerror = function(error) {
+	console.log("Error " + error.message);
+};
